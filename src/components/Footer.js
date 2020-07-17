daba1a8dca0c665ca09e0872e49cf72064737129
lalebdi <loai.alebdi@gmail.com>
2020-07-17 13:59:08 -0400

    we're ready to rock

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer>Place the footer here</footer>
+            </div>
+        )
+    }
+}
+
+export default Footer
---
+++ src/components/TopBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TopBar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Top Bar here</h1>
+            </div>
+        )
+    }
+}
+
+export default TopBar
