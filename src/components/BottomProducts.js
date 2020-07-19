2eed6255eea29a831ab317af2ce38e752564d41f
lalebdi <loai.alebdi@gmail.com>
2020-07-19 18:07:44 -0400

    Making progress with in the bottom part

--- src/components/BottomProducts.js
+++ src/components/BottomProducts.js
@@ -5,6 +5,7 @@ import Item2 from './assets/images/for_all_products/Aesop-Skin-In-Two-Minds-Faci
 import Item3 from './assets/images/for_all_products/Aesop-Skin-Mandarin-Facial-Hydrating-Cream-60mL-large.png';
 import Item4 from './assets/images/for_all_products/Aesop-SkinParsley-Seed-Facial-Cleanser-100mL-large.png';
 import Item5 from './assets/images/for_all_products/AesopSkin-B-Tea-Balancing-Toner-200mL-large.png';
+import Item6 from './assets/images/Aesop-Kits-Nashville-with-Product-Large-888x776px.png';
 
 
 
@@ -28,7 +29,9 @@ import Item5 from './assets/images/for_all_products/AesopSkin-B-Tea-Balancing-To
             </div>
           </div>
           <div className ="middle-section">
-          <div className ="left-side">bottle pic here</div>
+          <div className ="left-side">
+            <img className="imgs" src = {Item6} alt = "Nashville"/>
+          </div>
               <div className ="right-side">
                 <h4>Discover more</h4>
               </div>
