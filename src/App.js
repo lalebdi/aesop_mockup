0fee688032da0ad3e89241ea1728dadf79af0695
Naomi Isbell <naomiisbell@gmail.com>
2020-07-19 15:50:09 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Products2 from './components/Products2'
 import Kits2 from './components/Kits2'
 import Products3 from './components/Products3'
+import Products1 from './components/Products1';
+import Kits1 from './components/Kits1'
 
 function App() {
   return (
     <div className="App">
+      <Products1 />
+      <Kits1 />
       <Products2 />
       <Kits2 />
       <Products3 />
